fdc3a497246e7a6c52133043d1061defaabd538d
goldmojo <goldmojo@free.fr>
2021-02-06 16:38:46 +0100

    Adjust Makefile for new beta

--- bin/make_opk.sh
+++ bin/make_opk.sh
@@ -3,5 +3,5 @@
 rm -f fba.opk
 rm -f fba_ui_only.opk
 
-mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_explorer.gcw0.desktop aliases.txt fba.opk -all-root -no-xattrs -noappend -no-exports
-mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_ui_only.opk -all-root -no-xattrs -noappend -no-exports
+mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_explorer.gcw0.desktop aliases.txt fba.opk -all-root -no-xattrs -noappend -no-exports -force-uid 1000 -force-gid 100
+mksquashfs skin fbasdl.dge skin/fba_icon.png fba_ux.gcw0.desktop fba_ui_only.opk -all-root -no-xattrs -noappend -no-exports -force-uid 1000 -force-gid 100
